3827b597fedefaecff46127396c5b11d5a488bb5
acueducto <sotojohanestiven@gmail.com>
2025-07-03 19:05:11 -0500

    final de confirmacion

--- src/pages/Confirmacion/confirmacion.jsx
+++ src/pages/Confirmacion/confirmacion.jsx
@@ -7,14 +7,11 @@ const Confirmacion = () => {
 
   return (
     <div className="confirmacion-container">
-      <h2>SOLICITUD DE PETICIÓN</h2>
+      <h2>SOLICITUD</h2>
       <div className="mensaje">
         <p><strong>Tu solicitud ha sido enviada exitosamente.</strong></p>
         <p>Muchas gracias, nos estaremos comunicando.</p>
         <button className="boton-regresar" onClick={() => navigate('/')}>REGRESAR</button>
-      </div>
-      <div className="imagen-esquina">
-        <img className='img' src="/icons/Imagen1.jpg" alt="Decoración" />
       </div>
     </div>
   );
