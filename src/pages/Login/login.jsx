d860f59d9d62f80a6c9763eda790262a264d5dbc
Estiven Soto <sotojohanestiven@gmail.com>
2025-07-17 21:52:31 -0500

    cambios de color

--- src/pages/Login/login.jsx
+++ src/pages/Login/login.jsx
@@ -1,67 +1,64 @@
 import { useState } from 'react';
 import './login.css';
 
-
 function Login({ onLogin }) {
-    const [usuario, setUsuario] = useState('');
-    const [clave, setClave] = useState('');
-    const [error, setError] = useState('');
+  const [usuario, setUsuario] = useState('');
+  const [clave, setClave] = useState('');
+  const [error, setError] = useState('');
 
-    const manejarEnvio = (e) => {
-        e.preventDefault();
-        if (usuario === 'admin' && clave === '1234') {
-            onLogin();
-        } else {
-            setError('Usuario o contraseña incorrectos');
-        }
-    };
+  const manejarEnvio = (e) => {
+    e.preventDefault();
+    if (usuario === 'admin' && clave === '1234') {
+      onLogin();
+    } else {
+      setError('Usuario o contraseña incorrectos');
+    }
+  };
 
-    return (
-        <div className="contenedor-principal">
-            <div className="texto-bienvenida">
-                <img className="imagen-inferior" src='/imagenes/logo.jpg' alt="Decoración" />
-                <h1 className='titulo1'>
-                    Bienvenido a nuestro canal oficial de PQRS!<br />
-                    Tu opinión es fundamental para mejorar cada día.<br />
-                    <span>Estamos aquí para escucharte y responderte con transparencia y compromiso.</span>
-                </h1>
-            </div>
+  return (
+    <div className="contenedor-principal">
+      <div className="texto-bienvenida">
+        <h1 className="titulo1">
+          Bienvenido a nuestro canal oficial de PQRS!<br />
+          Tu opinión es fundamental para mejorar cada día.<br />
+          <span>Estamos aquí para escucharte y responderte con transparencia y compromiso.</span>
+        </h1>
+      </div>
 
-            <form onSubmit={manejarEnvio} className="formulario-login">
-                <h2>Iniciar Sesión</h2>
-                <div className='inicio'>
-                    <input
-                        type="text"
-                        placeholder="Email - Número de teléfono"
-                        value={usuario}
-                        onChange={(e) => setUsuario(e.target.value)}
-                        required
-                    />
-                    <input
-                        type="password"
-                        placeholder="Contraseña"
-                        value={clave}
-                        onChange={(e) => setClave(e.target.value)}
-                        required
-                    />
-                    <div class="botones">
-                        <button type="submit" class="btn-login">Iniciar Sesión</button>
-                        <button type="submit" class="btn-register">Regístrate</button>
-                    </div>
+      <form onSubmit={manejarEnvio} className="formulario-login">
+        <h2>Iniciar Sesión</h2>
+        <div className="inicio">
+          <input
+            type="text"
+            placeholder="Email - Número de teléfono"
+            value={usuario}
+            onChange={(e) => setUsuario(e.target.value)}
+            required
+          />
+          <input
+            type="password"
+            placeholder="Contraseña"
+            value={clave}
+            onChange={(e) => setClave(e.target.value)}
+            required
+          />
+          <div className="botones">
+            <button type="submit" className="btn-login">Iniciar Sesión</button>
+            <button type="button" className="btn-register">Regístrate</button>
+          </div>
 
-                    {error && <p>{error}</p>}
-                </div>
-                <div className="footer-links">
-                    <a href="#">Términos de Servicios</a>
-                    <a href="#">Política de privacidad</a>
-                </div>
-            </form>
-
-
-
-
+          {error && <p>{error}</p>}
         </div>
-    );
+
+        <div className="footer-links">
+          <a href="#">Términos de Servicios</a>
+          <a href="#">Política de privacidad</a>
+        </div>
+      </form>
+
+      <img className="imagen-inferior" src="/imagenes/logo.jpg" alt="Decoración" />
+    </div>
+  );
 }
 
 export default Login;
