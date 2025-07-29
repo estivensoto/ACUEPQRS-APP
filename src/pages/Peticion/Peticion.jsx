deab8042fef480d47efc7da3d830343190c45acb
Estiven Soto <sotojohanestiven@gmail.com>
2025-07-29 13:18:44 -0500

    no sale en pantalla

--- src/pages/Peticion/Peticion.jsx
+++ src/pages/Peticion/Peticion.jsx
@@ -1,7 +1,6 @@
 import React, { useState } from 'react';
 import { Link, useNavigate } from 'react-router-dom';
 import './peticion.css';
-import PeticionImg from '../../assets/peticiones.png'; // Importar la imagen si es necesario
 
 const Peticion = () => {
     const navigate = useNavigate();
@@ -23,22 +22,21 @@ const Peticion = () => {
 
     const handleSubmit = (e) => {
         e.preventDefault();
-        console.log(formData);
+        console.log(formData); // Confirmar que llega la data
         navigate('/confirmacion');
     };
 
     return (
         <div>
             <nav style={{ textAlign: 'center', margin: '20px' }}>
-                <Link className='iten' to="/">🏚 Inicio</Link> {' '}
+                <Link className='iten' to="/">🏚 Inicio</Link>
             </nav>
+
             <div className="peticion-container">
-                <div className='titulo'><h1>PETICIONES</h1>
-                <div className='img'>
-                        <img src={PeticionImg} alt="bobada" />
-                    </div>
+                <div className="titulo">
+                    <h1>PETICIONES</h1>
                 </div>
-                
+
                 <form className="peticion-form" onSubmit={handleSubmit}>
                     <input type="text" name="nombres" placeholder="Nombres" onChange={handleChange} required />
                     <input type="text" name="apellidos" placeholder="Apellidos" onChange={handleChange} required />
@@ -50,11 +48,9 @@ const Peticion = () => {
                     <textarea name="hechos" placeholder="Hechos" onChange={handleChange}></textarea>
                     <textarea name="pretensiones" placeholder="Pretensiones" onChange={handleChange}></textarea>
                     <button type="submit">📄 Generar</button>
-                    
                 </form>
             </div>
         </div>
-
     );
 };
 
